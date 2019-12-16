91d0ae7a0acbcec92e0e83da82637e4848470de5
Ethan Kieser <53837375+ekieser@users.noreply.github.com>
2019-12-15 22:58:47 -0600

    added map compatibility

--- index.js
+++ index.js
@@ -1,51 +1,23 @@
 'use strict';
 
+const searchURL = 'https://api.openbrewerydb.org/breweries';
+
+const mapURL = 'https://www.google.com/maps/search/?api=1&query=';
+
 function retrieveCity() {
     const cityValue = $('#city-entry').val().toLowerCase();
-    const url = `https://api.openbrewerydb.org/breweries?by_city=${cityValue}`;
-    fetch (url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then (responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
-}
-
-function retrieveState() {
-    const stateValue = $('#state-entry').val().toLowerCase();
-    const url = `https://api.openbrewerydb.org/breweries?by_state=${stateValue}`;
-    fetch (url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-         .then (responseJson => displayResults(responseJson))
-         .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
-}
-
-function retrieveName() {
-    const nameValue = $('#name-entry').val().toLowerCase();
-    const url = `https://api.openbrewerydb.org/breweries?by_name=${nameValue}`;
+    const url = searchURL + '?by_city=' + `${cityValue}` + '&per_page=50';
     fetch(url)
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error(response.statusText);
-        })
-        .then (responseJson => displayResults(responseJson))
-        .catch(err => {
-            $('#js-error-message').text(`Something went wrong: ${err.message}`);
-        });
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then (responseJson => displayResults(responseJson))
+    .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    })
 }
 
 function displayResults(responseJson) {
@@ -53,11 +25,10 @@ function displayResults(responseJson) {
     for (let i = 0; i < responseJson.length; i++) {
         $('#display-results').append(
             `<h3>${responseJson[i].name}</h3>
-            <p>${responseJson[i].street}</p>
-            <p>${responseJson[i].city}, ${responseJson[i].state}</p>
-            <p>${responseJson[i].brewery_type}</p>
+            <p><a href="${mapURL}${responseJson[i].name}" target="_blank">${responseJson[i].street}, ${responseJson[i].city}, ${responseJson[i].state}</a></p>
+            <p>Brewery type: ${responseJson[i].brewery_type}</p>
             <p>Phone number: ${responseJson[i].phone}</p>
-            <p>Website: ${responseJson[i].website_url}</p>
+            <p>Website: <a href="${responseJson[i].website_url}" target="_blank">${responseJson[i].website_url}</a></p>
             <p>${responseJson[i].tag_list}</p>`
         )
     };
@@ -69,14 +40,9 @@ function beginSearch() {
         console.clear();
         $('#display-results').html('');
         $('#js-error-message').html('');
-        const nameSearch = $('#name-entry').val();
-        const stateSeacrh = $('#state-entry').val();
         const citySearch = $('#city-entry').val();
-        retrieveName(nameSearch);
         retrieveCity(citySearch);
-        retrieveCity(stateSeacrh);
     });
 }
 
 $(beginSearch);
-
